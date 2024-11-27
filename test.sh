384fafe117f2166cc00d51b780a53aa3af1bc3c2
Amelia Reese <108507479+ameliar67@users.noreply.github.com>
2024-11-27 12:01:27 -0800

    Test with another spelling error

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 ## Example: a typical script with several problems
 for f in $(ls *.m3u)
 do
-  grexp -qi hq.*mp3 $f \
+  grex -qi hq.*mp3 $f \
     && echo -e 'Playlist $f contains a HQ file in mp3 format'
 
 
